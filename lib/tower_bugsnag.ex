14efede010fab4ea4ce1b7400859f6d6f7ed91d2
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-10-01 16:00:18 -0300

    mix new tower_bugsnag

---
+++ lib/tower_bugsnag.ex
@@ -0,0 +1,18 @@
+defmodule TowerBugsnag do
+  @moduledoc """
+  Documentation for `TowerBugsnag`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TowerBugsnag.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tower_bugsnag_test.exs
@@ -0,0 +1,8 @@
+defmodule TowerBugsnagTest do
+  use ExUnit.Case
+  doctest TowerBugsnag
+
+  test "greets the world" do
+    assert TowerBugsnag.hello() == :world
+  end
+end
