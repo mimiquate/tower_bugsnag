0ae52b0268ec3743668b1fd78a15583b5abff609
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-10-02 17:36:19 -0300

    fix: ignore but handle tower messages

--- lib/tower_bugsnag/reporter.ex
+++ lib/tower_bugsnag/reporter.ex
@@ -1,4 +1,12 @@
 defmodule TowerBugsnag.Reporter do
+  def report_event(%Tower.Event{kind: :message}) do
+    if enabled?() do
+      IO.puts("Bugsnag DOES NOT support reporting messages, ignoring...")
+    else
+      IO.puts("TowerBugsnag NOT enabled, ignoring...")
+    end
+  end
+
   def report_event(%Tower.Event{} = tower_event) do
     if enabled?() do
       tower_event
