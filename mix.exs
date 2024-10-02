2691c6a1b934f052241d05aa8b0bae2806245969
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-10-02 10:26:42 -0300

    prepares for hex package build

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule TowerBugsnag.MixProject do
   use Mix.Project
 
+  @description "Error tracking and reporting to BugSnag"
+  @source_url "https://github.com/mimiquate/tower_bugsnag"
+  @version "0.1.0"
+
   def project do
     [
       app: :tower_bugsnag,
-      version: "0.1.0",
+      description: @description,
+      version: @version,
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -27,6 +33,15 @@ defmodule TowerBugsnag.MixProject do
 
       # Test
       {:bypass, github: "mimiquate/bypass", only: :test}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => @source_url
+      }
     ]
   end
 end
