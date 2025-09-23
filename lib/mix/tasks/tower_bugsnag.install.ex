0e3e68a64a18afe4d90c821b95b52cc61ac36e81
Gonzalo R <456459+grzuy@users.noreply.github.com>
2025-09-23 11:57:19 -0300

    refactor(igniter): uses `Tower.Igniter.runtime_configure_reporter/3` (#65)

--- lib/mix/tasks/tower_bugsnag.install.ex
+++ lib/mix/tasks/tower_bugsnag.install.ex
@@ -1,4 +1,6 @@
-if Code.ensure_loaded?(Igniter) && Code.ensure_loaded?(Tower.Igniter) do
+if Code.ensure_loaded?(Igniter) and
+     Code.ensure_loaded?(Tower.Igniter) and
+     function_exported?(Tower.Igniter, :runtime_configure_reporter, 3) do
   defmodule Mix.Tasks.TowerBugsnag.Install do
     @example "mix igniter.install tower_bugsnag"
 
@@ -15,143 +17,19 @@ if Code.ensure_loaded?(Igniter) && Code.ensure_loaded?(Tower.Igniter) do
 
     use Igniter.Mix.Task
 
-    @default_runtime_config """
-    import Config
-
-    if config_env() == :prod do
-      config :tower_bugsnag, api_key: System.get_env("BUGSNAG_API_KEY")
-    end
-    """
-
-    @prod_config_patterns [
-      """
-      if config_env() == :prod do
-        __cursor__()
-      end
-      """,
-      """
-      if :prod == config_env() do
-        __cursor__()
-      end
-      """
-    ]
-
-    @bugsnag_config_code """
-    config :tower_bugsnag, api_key: System.get_env("BUGSNAG_API_KEY")
-    """
-
     @impl Igniter.Mix.Task
     def info(_argv, _composing_task) do
-      %Igniter.Mix.Task.Info{
-        group: :tower,
-        adds_deps: [],
-        installs: [],
-        example: @example,
-        only: nil,
-        positional: [],
-        composes: [],
-        schema: [],
-        defaults: [],
-        aliases: [],
-        required: []
-      }
+      %Igniter.Mix.Task.Info{group: :tower, example: @example}
     end
 
     @impl Igniter.Mix.Task
     def igniter(igniter) do
       igniter
       |> Tower.Igniter.reporters_list_append(TowerBugsnag)
-      |> configure_runtime()
-    end
-
-    defp configure_runtime(igniter) do
-      if runtime_config_exists?(igniter) do
-        igniter
-      else
-        add_runtime_config(igniter)
-      end
-    end
-
-    defp runtime_config_exists?(igniter) do
-      Igniter.Project.Config.configures_key?(igniter, "runtime.exs", :tower_bugsnag, [
-        :api_key
-      ])
-    end
-
-    defp add_runtime_config(igniter) do
-      Igniter.create_or_update_elixir_file(
-        igniter,
-        "config/runtime.exs",
-        @default_runtime_config,
-        &update_runtime_config/1
-      )
-    end
-
-    defp update_runtime_config(zipper) do
-      if Igniter.Project.Config.configures_key?(zipper, :tower_bugsnag, :api_key) do
-        {:ok, zipper}
-      else
-        add_config_to_prod_block(zipper)
-      end
-    end
-
-    defp add_config_to_prod_block(zipper) do
-      zipper
-      |> Igniter.Code.Common.move_to_cursor_match_in_scope(@prod_config_patterns)
-      |> case do
-        {:ok, zipper} ->
-          handle_existing_prod_block(zipper)
-
-        :error ->
-          add_prod_block_with_config(zipper)
-      end
-    end
-
-    defp handle_existing_prod_block(zipper) do
-      if Igniter.Project.Config.configures_key?(zipper, :tower_bugsnag, :api_key) do
-        {:ok, zipper}
-      else
-        update_existing_config_or_add_new(zipper)
-      end
-    end
-
-    defp update_existing_config_or_add_new(zipper) do
-      case find_existing_bugsnag_config(zipper) do
-        {:ok, _zipper} ->
-          update_existing_bugsnag_config(zipper)
-
-        _ ->
-          Igniter.Code.Common.add_code(zipper, @bugsnag_config_code)
-      end
-    end
-
-    defp find_existing_bugsnag_config(zipper) do
-      Igniter.Code.Function.move_to_function_call_in_current_scope(
-        zipper,
-        :=,
-        2,
-        fn call ->
-          Igniter.Code.Function.argument_equals?(call, 0, :tower_bugsnag)
-        end
-      )
-    end
-
-    defp update_existing_bugsnag_config(zipper) do
-      zipper
-      |> Igniter.Project.Config.modify_config_code(
-        [:api_key],
+      |> Tower.Igniter.runtime_configure_reporter(
         :tower_bugsnag,
-        Sourceror.parse_string!(~s[System.get_env("BUGSNAG_API_KEY")])
+        api_key: {:code, Sourceror.parse_string!(~s[System.get_env("BUGSNAG_API_KEY")])}
       )
-      |> then(&{:ok, &1})
-    end
-
-    defp add_prod_block_with_config(zipper) do
-      Igniter.Code.Common.add_code(zipper, """
-      if config_env() == :prod do
-        #{@bugsnag_config_code}
-      end
-      """)
     end
   end
 else
@@ -175,7 +53,7 @@ else
     @impl Mix.Task
     def run(_argv) do
       Mix.shell().error("""
-      The task 'tower_bugsnag.install' requires igniter and tower > 0.8.3. Please install igniter or update tower and try again.
+      The task 'tower_bugsnag.install' requires igniter and tower >= 0.8.4. Please install igniter or update tower and try again.
 
       For more information, see: https://hexdocs.pm/igniter/readme.html#installation
       """)
